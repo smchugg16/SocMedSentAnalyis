c2bd7652c7d4a4f2e260f88deeb74f5b35f27ae9
Sean <125521552+smchugg16@users.noreply.github.com>
2023-12-05 12:07:21 -0500

    Add files via upload

--- Data Analysis.sql
+++ Data Analysis.sql
@@ -1,50 +1,48 @@
 
-/*
-Most Occuring Words
-*/
+--- Retrieve the top 200 most occurring words and the number of occurences as 'TotalFrequency' from the CombinedSentimentTable ---
 SELECT TOP 200
     Word,
     SUM(Frequency) AS TotalFrequency
 FROM
-    CombinedSentimentTable
+    SocMedia.dbo.CombinedSentimentTable
 GROUP BY
     Word
 ORDER BY
     TotalFrequency DESC;
 
 
---Creating a new Sentiment Score of positive/negative--
+--- Creating a new Sentiment Score based on positive and negative categories, then selecting the Absolute Value of these scores to find the Top 200 strongest sentiments ---
 SELECT TOP 200
     Word,
     SUM(Frequency * CASE WHEN Category = 'Positive' THEN 1 WHEN Category = 'Negative' THEN -1 ELSE 0 END) AS CustomSentimentScore
 FROM
-    CombinedSentimentTable
+    SocMedia.dbo.CombinedSentimentTable
 GROUP BY
     Word
 ORDER BY
     ABS(SUM(Frequency * CASE WHEN Category = 'Positive' THEN 1 WHEN Category = 'Negative' THEN -1 ELSE 0 END)) DESC;
 
 
---Sentiments of most popular words--
+
+--- Calculate new sentiment scores (same as above) for the top 200 most popular words ---
 SELECT TOP 200
     Word,
     SUM(Frequency * CASE WHEN Category = 'Positive' THEN 1 WHEN Category = 'Negative' THEN -1 ELSE 0 END) AS CustomSentimentScore,
 	SUM(Frequency) AS TotalFrequency
 FROM
-    CombinedSentimentTable
+    SocMedia.dbo.CombinedSentimentTable
 GROUP BY
     Word
 ORDER BY
 	TotalFrequency DESC;
 
 
-/*
-*/
+--- Count the occurrences of each sentiment category (positive/negative/neutral) in the CombinedSentimentTable ---
 SELECT
     Category,
     COUNT(*) AS Frequency
 FROM
-    CombinedSentimentTable
+    SocMedia.dbo.CombinedSentimentTable
 GROUP BY
     Category;
 
